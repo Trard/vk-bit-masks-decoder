6f7331ea6fa340a33dc27d3f53deda8763f6846a
Trard <rivaavir3@gmail.com>
2021-06-08 22:43:31 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const permissions = {
+    user: ["notify", "friends", "photos", "audio", "video", null, "stories", "pages", "add_app_to_left_menu", null, "status", "notes", "messages", "wall", null, "ads", "offline", "docs", "groups", "notifications", "stats", null, "email", null, null, null, null, "market"],
+    group: ["stories", null, "photos", null, null, null, "app_widget", null, null, null, null, null, "messages", null, null, null, null, "docs", "manage"],
+}
+
+const decodeBitMask = (bitMask, permArray) => {
+    let result = []
+
+    let binMaskString = bitMask.toString(2)
+    let binMaskArray = [...binMaskString]
+
+    binMaskArray.map((bin, index) => {
+        let perm = permArray[index]
+        if (bin === "1" && perm !== null) {
+            result.push(permArray[index])
+        }
+    })
+
+    return result;
+}
+
+module.exports = { permissions, decodeBitMask }
